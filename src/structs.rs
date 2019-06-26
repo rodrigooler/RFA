19707c079473bafedfa4122fefa3de0161a94dfc
Oler <roodrigoprogrammer@gmail.com>
2019-06-26 03:08:19 -0300

    create src/structs.rs

---
+++ src/structs.rs
@@ -0,0 +1,15 @@
+#[derive(Serialize, Deserialize)]
+pub struct Brazil {
+    pub id: String,
+    pub city: String,
+    pub country: String,
+    pub neighborhood: String,
+    pub state: String,
+    pub street: String,
+    pub uf: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DB {
+    pub brazil: Vec<Brazil>
+}
